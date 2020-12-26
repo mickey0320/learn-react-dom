4a15263b85a18467ba922db1f3fa278e4dcd1621
yanjian <jian.yan@ishansong.com>
2020-12-26 14:09:10 +0800

    实现history库

--- src/react-router/index.js
+++ src/react-router/index.js
@@ -4,3 +4,4 @@ export { default as Route } from "./Route";
 export { default as Switch } from "./Switch";
 export { default as Redirect } from "./Redirect";
 export { default as withRouter } from "./withRouter";
+export { default as Prompt } from "./Prompt";
